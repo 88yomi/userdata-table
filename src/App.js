f3732819d0867372a75b41052eba4570e666edf4
unknown <yomibalo20@gmail.com>
2022-04-16 12:47:23 +0100

    improved firebase upload and link retrieval

--- src/App.js
+++ src/App.js
@@ -7,53 +7,45 @@ import Form from './components/Form';
 import Header from './components/Header';
 import Table from './components/Table';
 
-import { ref, getDownloadURL, uploadBytesResumable, uploadBytes } from 'firebase/storage'; //
-import { storage } from './firebase/config'; //
+import { ref, getDownloadURL, uploadBytesResumable } from 'firebase/storage';
+import { storage } from './firebase/config';
 
 function App() {
   const [submitData, setSubmitData] = useState([]);
-  
-  const [imgUrl, setImgUrl] = useState(null);
-  
-  const [progress, setProgress] = useState(0);
 
   const uploadImage = (image) => {
-    const storageRef = ref(storage, `images/${image.name}`);
-    const uploadTask = uploadBytesResumable(storageRef, image);
+    return new Promise((resolve, reject) => {
+      const storageRef = ref(storage, `images/${image.name}`);
+      const uploadTask = uploadBytesResumable(storageRef, image);
 
-    uploadTask.on('state_changed', (snapshot) => {
-      const percent = Math.round(snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-      setProgress(percent);
-      console.log(progress)
-    },
-      (error) => {
-        console.log(error);
-      },
-      () => {
-        getDownloadURL(uploadTask.snapshot.ref)
-          .then((url) => setImgUrl(url));
-      })
+      uploadTask.on('state_changed', () => { },
+        (error) => {
+          reject(error)
+          console.log(error);
+        },
+        () => {
+          resolve(getDownloadURL(uploadTask.snapshot.ref));
+        })
+    })
   }
 
-  const handleFormSubmit = async (e) => {
+  const handleFormSubmit = (e) => {
     e.preventDefault();
 
-    const file = e.target[4].files[0];
-    setImgUrl(uploadImage(file))
-
-    let photo = await uploadImage(e.target[4].value);
-    console.log(photo);
-
-    let newData = {
+    const rawFormData = {
       name: e.target[0].value,
       email: e.target[1].value,
       phone: e.target[2].value,
       hungry: e.target[3].value,
-      photo: e.target[4].value,
-      id: nanoid(10)
-    };
-    setSubmitData(submitData => [...submitData, newData]);
+      photo: e.target[4].files[0],
+    }
+
     document.querySelectorAll('input').forEach(elem => elem.value = '');
+    uploadImage(rawFormData.photo)
+      .then(imgLink => {
+        const newData = { ...rawFormData, photo: imgLink }
+        setSubmitData(submitData => [...submitData, newData]);
+      })
   }
 
   // Routing
